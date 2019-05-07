9f82e76b56cfc286815adf35c04dc9dd2e4f8b2e
hahame <hahamezero@163.com>
2019-05-08 00:37:08 +0800

    update

--- view/src/main/java/com/felix/tools/view/loading/UIStatus.java
+++ view/src/main/java/com/felix/tools/view/loading/UIStatus.java
@@ -1,8 +1,6 @@
 package com.felix.tools.view.loading;
 
-import android.content.Context;
 import android.os.Build;
-import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,22 +14,26 @@ public class UIStatus {
     public static final int STATUS_LOAD_SUCCESS = 2;
     public static final int STATUS_LOAD_FAILED = 3;
     public static final int STATUS_EMPTY_DATA = 4;
+    private SparseArray<View> mStatusViews = new SparseArray<>(4);
 
-    private StatusAdapter mAdapter;
+    private ViewGroup mRootView;
 
-    public interface StatusAdapter {
-        View getView(Holder holder, View convertView, int status);
+    public interface StatusViewAdapter {
+        View getView(int status);
     }
 
-    public UIStatus() {
+    private UIStatus() {
     }
 
-    public UIStatus setAdapter(StatusAdapter adapter) {
-        this.mAdapter = adapter;
-        return this;
+    private Builder mBuilder;
+
+    private UIStatus(Builder builder) {
+        this.mBuilder = builder;
+        this.mRootView = wrap(this.mBuilder.mContentView);
+        this.mStatusViews.put(STATUS_LOAD_SUCCESS, this.mRootView);
     }
 
-    public Holder wrap(View view) {
+    private ViewGroup wrap(View view) {
         FrameLayout wrapper = new FrameLayout(view.getContext());
         ViewGroup.LayoutParams lp = view.getLayoutParams();
         if (lp != null) {
@@ -45,142 +47,84 @@ public class UIStatus {
         }
         LayoutParams newLp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         wrapper.addView(view, newLp);
-        return new Holder(mAdapter, view.getContext(), wrapper);
+        return wrapper;
     }
 
 
-    public static class Holder {
-        private StatusAdapter mAdapter;
-        private Context mContext;
-        private Runnable mRetryTask;
-        private View mCurStatusView;
-        private ViewGroup mWrapper;
-        private int curState;
-        private SparseArray<View> mStatusViews = new SparseArray<>(4);
-        private Object mData;
+    public void showLoading() {
+        View loadingView = mStatusViews.get(STATUS_LOADING);
+        if (null == loadingView) {
+            loadingView = this.mBuilder.mStatusViewAdapter.getView(STATUS_LOADING);
+            mStatusViews.put(STATUS_LOADING, loadingView);
+        }
+        showViewByStatus(STATUS_LOADING);
+    }
 
-        private Holder(StatusAdapter adapter, Context context, ViewGroup wrapper) {
-            this.mAdapter = adapter;
-            this.mContext = context;
-            this.mWrapper = wrapper;
+
+    public void showLoadSuccess() {
+        showViewByStatus(STATUS_LOAD_SUCCESS);
+    }
+
+
+    public void showLoadFailed() {
+        showViewByStatus(STATUS_LOAD_FAILED);
+    }
+
+
+    public void showEmpty() {
+        showViewByStatus(STATUS_EMPTY_DATA);
+    }
+
+
+    public void showViewByStatus(int status) {
+        final int childCount = mStatusViews.size();
+        for (int i = 0; i < childCount; i++) {
+            if (status == mStatusViews.keyAt(i)) {
+                mStatusViews.valueAt(i).setVisibility(View.VISIBLE);
+            } else {
+                mStatusViews.valueAt(i).setVisibility(View.GONE);
+            }
+        }
+    }
+
+
+    public static class Builder {
+        public Runnable mRetryTask;
+        public View mContentView;
+        public StatusViewAdapter mStatusViewAdapter;
+
+        public Builder content(View contentView) {
+            this.mContentView = contentView;
+            return this;
         }
 
+        public Builder statusViewAdapter(StatusViewAdapter adapter) {
+            this.mStatusViewAdapter = adapter;
+            return this;
+        }
 
-        public Holder withRetry(Runnable task) {
+        public Builder withRetry(Runnable task) {
             mRetryTask = task;
             return this;
         }
 
-        public Holder withData(Object data) {
-            this.mData = data;
-            return this;
-        }
-
-        public void showLoading() {
-            showLoadingStatus(STATUS_LOADING);
+        public UIStatus build() {
+            validate();
+            return new UIStatus(this);
         }
 
 
-        public void showLoadSuccess() {
-            showLoadingStatus(STATUS_LOAD_SUCCESS);
-        }
-
-
-        public void showLoadFailed() {
-            showLoadingStatus(STATUS_LOAD_FAILED);
-        }
-
-
-        public void showEmpty() {
-            showLoadingStatus(STATUS_EMPTY_DATA);
-        }
-
-
-        public void showLoadingStatus(int status) {
-            if (curState == status || !validate()) {
-                return;
+        private void validate() {
+            if (mStatusViewAdapter == null) {
+                throw new IllegalStateException("StatusViewAdapter is not specified.");
             }
-            curState = status;
-            //first try to reuse status view
-            View convertView = mStatusViews.get(status);
-            if (convertView == null) {
-                //secondly try to reuse current status view
-                convertView = mCurStatusView;
-            }
-            try {
-                //call customer adapter to get UI for specific status. convertView can be reused
-                View view = mAdapter.getView(this, convertView, status);
-                if (view == null) {
-                    printLog(mAdapter.getClass().getName() + ".getView returns null");
-                    return;
-                }
-                if (view != mCurStatusView || mWrapper.indexOfChild(view) < 0) {
-                    if (mCurStatusView != null) {
-                        mWrapper.removeView(mCurStatusView);
-                    }
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        view.setElevation(Float.MAX_VALUE);
-                    }
-                    mWrapper.addView(view);
-                    ViewGroup.LayoutParams lp = view.getLayoutParams();
-                    if (lp != null) {
-                        lp.width = ViewGroup.LayoutParams.MATCH_PARENT;
-                        lp.height = ViewGroup.LayoutParams.MATCH_PARENT;
-                    }
-                } else if (mWrapper.indexOfChild(view) != mWrapper.getChildCount() - 1) {
-                    // make sure loading status view at the front
-                    view.bringToFront();
-                }
-                mCurStatusView = view;
-                mStatusViews.put(status, view);
-            } catch (Exception e) {
-                if (DEBUG) {
-                    e.printStackTrace();
-                }
+            if (mContentView == null) {
+                throw new IllegalStateException("ContentView is not specified.");
             }
         }
 
-        private boolean validate() {
-            if (mAdapter == null) {
-                printLog("StatusAdapter is not specified.");
-            }
-            if (mContext == null) {
-                printLog("Context is null.");
-            }
-            if (mWrapper == null) {
-                printLog("The mWrapper of loading status view is null.");
-            }
-            return mAdapter != null && mContext != null && mWrapper != null;
-        }
 
-        public Context getContext() {
-            return mContext;
-        }
-
-        public ViewGroup getWrapper() {
-            return mWrapper;
-        }
-
-        public Runnable getRetryTask() {
-            return mRetryTask;
-        }
-
-
-        public <T> T getData() {
-            try {
-                return (T) mData;
-            } catch (Exception e) {
-                if (DEBUG) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
-        }
     }
 
-    private static void printLog(String msg) {
-        if (DEBUG) {
-            Log.e("Gloading", msg);
-        }
-    }
+
 }
